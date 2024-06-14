2a6728312f3176800299b67ff00a1a0c3a13b9a0
teacherMaik <d.michaelthomasbennett@gmail.com>
2024-06-14 22:39:27 +0200

    drop down menu in work

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 const chalMenuBtn = document.getElementById('challenges-btn');
+const chalBtnContainer = document.getElementById('challenges-btn-container');
+const dropDownNav = document.querySelector('.nav-drop-down');
+
+chalMenuBtn.addEventListener('click', (e) => {
+
+  e.preventDefault();
+
+  if (!chalBtnContainer.classList.contains('open')) {
+
+    chalBtnContainer.classList.add('open');
+    dropDownNav.style.display = 'block';
+  } else {
+    chalBtnContainer.classList.remove('open');
+    dropDownNav.style.display = 'none';
+  }
+
+
+});
