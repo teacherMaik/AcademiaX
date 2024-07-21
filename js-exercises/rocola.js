d45be4eb4a73c123c548f05ca94f3a2535690fa7
teacherMaik <d.michaelthomasbennett@gmail.com>
2024-07-22 00:13:57 +0200

    badass rocola with desired animations and evertyhing

--- js-exercises/rocola.js
+++ js-exercises/rocola.js
@@ -59,7 +59,7 @@ class Queue {
     }
     else
     {
-      return this.songs.shift();
+      this.songs.shift();
     }
   }
 
@@ -85,12 +85,32 @@ function randNum(songsIndexed) {
   return newNum;
 }
 
-function playSongs(numSongs) {
+function delay(ms) {
+
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function animateIn(songDisplayed, string) {
+
+
+  upNext.innerHTML = string;
+}
+
+function setSongList(songDisplayed, string) {
+
+}
+
+function animateOut(songDisplayed, string) {
+
+}
+
+async function playSongs(numSongs) {
 
   let songsQueue = new Queue();
 
   if (numSongs > songs.length) {
 
+    alert('¡No tenemos tantas canciones!')
     return 'No tenemos tantas canciones';
   }
   else
@@ -105,48 +125,83 @@ function playSongs(numSongs) {
 
     while (songCount < numSongs) {
 
+      cassetteCircles.forEach(circle => {
+
+        circle.classList.add('active');
+      });
+
       if (songsIndexed.length >= 4) {
-        console.log('songsIndexed.length is ' + songsIndexed.length);
+        /* console.log('songsIndexed.length is ' + songsIndexed.length);
         console.log('songCount is ' + songCount);
-        console.log('second index is ' + songsIndexed.length);
+        console.log('second index is ' + songsIndexed.length); */
         let lastFourSongs = songsIndexed.slice(songsIndexed.length - 4, songsIndexed.length);
 
-        console.log('last four songs are ' + lastFourSongs);
+        /* console.log('last four songs are ' + lastFourSongs); */
 
         randIndex = randNum(lastFourSongs);
-        console.log('randIndex from lastFour is ' + randIndex);
+        /* console.log('randIndex from lastFour is ' + randIndex); */
         songsIndexed.push(randIndex);
-        console.log('songsIndexed after lastFour check is ' + songsIndexed);
+        /* console.log('songsIndexed after lastFour check is ' + songsIndexed); */
       }
       else if (songsIndexed.length < 4)
       {
         randIndex = randNum(songsIndexed);
-        console.log('rnad Index from else is ' + randIndex);
+        /* console.log('rnad Index from else is ' + randIndex); */
 
         songsIndexed.push(randIndex);
-        console.log('songsIndexed is ' + songsIndexed);
+        /* console.log('songsIndexed is ' + songsIndexed); */
       }
 
       songsQueue.enqueue(songs[randIndex]);
-      console.log(songsQueue.songs);
-      console.log(songsIndexed);
       songCount++;
+    }
+
+
+    for (var j = 0; j < songsIndexed.length; j++) {
+
+      nowPlaying.classList.remove('out');
+      upNext.classList.remove('out');
+
+      await delay(420);
+      nowPlaying.innerHTML = songsQueue.peek().name + '<br/> played by ' + songsQueue.peek().group;
+      nowPlaying.classList.add('in');
+      
+      await delay(420);
+      songsQueue.dequeue();
+
+      if (j <= numSongs - 2) {
+        upNext.innerHTML = songsQueue.peek().name + '<br/> played by ' + songsQueue.peek().group;
+        upNext.classList.add('in');
+      }
+      else if (j === numSongs - 1) {
+        upNext.innerHTML = '';
+      }
+
+      await delay(1000);
+      nowPlaying.classList.remove('in');
+      nowPlaying.classList.add('out');
+
+      await delay(420);
+      upNext.classList.remove('in');
+      upNext.classList.add('out');
+
+      await delay(1000);
+      nowPlaying.innerHTML = '';
+      upNext.innerHTML = '';
+
     }
   }
 
-  console.log(songsQueue);
+  cassetteCircles.forEach(circle => {
+
+    circle.classList.remove('active');
+  });
 }
 
 
 
 playBtn.addEventListener('click', (e) => {
 
-  console.log(requestInput.value);
-  console.log(e.value);
-  cassetteCircles.forEach(circle => {
-
-    circle.classList.toggle('active');
-  });
-
-  playSongs(requestInput.value);
+  const songsToPlay = playSongs(requestInput.value);
+  
 });
