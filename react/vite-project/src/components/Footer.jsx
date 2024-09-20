5f402073a03b49a08edc98dc42b2432721971346
teacherMaik <d.michaelthomasbennett@gmail.com>
2024-09-20 22:32:24 +0200

    Vite created

---
+++ react/vite-project/src/components/Footer.jsx
@@ -0,0 +1,30 @@
+
+
+function Footer() {
+
+  return (
+    <footer>
+      <div id="footer-container">
+        <div id="footer-social">
+          <a href="https://github.com/teacherMaik" class="fab fa-github fa-4x" target="_blank"></a>
+          <a href="https://x.com/TeacherMaik" class="fab fa-twitter fa-4x" target="_blank"></a>
+          <a href="https://www.instagram.com/teacherMaik" class="fab fa-instagram fa-4x" target="_blank"></a>
+          <a href="https://www.linkedin.com/in/michael-thomas-bennett-6631822a/" class="fab fa-linkedin fa-4x" target="_blank"></a>
+          <a href="https://www.youtube.com/channel/UCLTvAgvSy7ViRi7yAe0ogDA" class="fab fa-youtube fa-4x" target="_blank"></a>
+        </div>
+        <div id="footer-info">
+          <p>&copy;Michael Thomas Bennett</p>
+        </div>
+      </div>
+      <div id="footer-container">
+        <div id="my-webs">
+          <a href="https://ludotecaenlanube.com/">Ludoteca en la Nube</a>
+          <a href="https://www.vindrogames.com/">Vindrogames</a>
+          <a href="https://terraturca.es/">Terra Turca</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
